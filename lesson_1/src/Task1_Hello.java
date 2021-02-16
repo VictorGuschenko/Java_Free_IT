70dd00d9369476bdb93f29d1e830e8dac3940c5f
Victor Guschenko <victor.guschenko@gmail.com>
2021-02-16 17:36:14 +0100

    lesson1

---
+++ lesson_1/src/Task1_Hello.java
@@ -0,0 +1,10 @@
+
+
+/**
+ * Набрать приведенный пример, откомпилировать его и запустить.
+ */
+public class Task1_Hello {
+    public static void main (String [] args) {
+        System.out.println("Hello World!");
+    }
+}
