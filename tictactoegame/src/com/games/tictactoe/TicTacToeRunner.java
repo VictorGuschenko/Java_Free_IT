319835c7133d799d4a3dc08fade8caf033b14343
Victor Guschenko <victor.guschenko@gmail.com>
2021-03-12 09:22:02 +0100

    Added avoid of incorrect input

--- tictactoegame/src/com/games/tictactoe/TicTacToeRunner.java
+++ tictactoegame/src/com/games/tictactoe/TicTacToeRunner.java
@@ -1,12 +1,20 @@
 package com.games.tictactoe;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Integer.parseInt;
 
 public class TicTacToeRunner {
     static int playerNumber;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char[][] field = new char[3][3];
+        Pattern regexp;
+        Matcher matcher;
+        String move;
         int count;
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
@@ -14,37 +22,51 @@ public class TicTacToeRunner {
             }
         }
         boolean isProceed = true;
-        System.out.println("Let's Play The TIC TAC TOE!");
+        System.out.println("Let's play the TIC TAC TOE!");
         while (isProceed) {
-            System.out.println("Press '1' if you wanna play for 'X' or 2 for '0'");
-            playerNumber = scanner.nextInt();
-            switch (playerNumber) {
-                case 1:
-                    System.out.println("You've chosen 'X'");
-                    isProceed = false;
-                    break;
-                case 2:
-                    System.out.println("You've chosen '0'");
-                    isProceed = false;
-                    break;
-                default:
-                    System.out.println("Wrong input. Should be 1 or 2");
+            System.out.println("Press '1' if you want to play for 'X'");
+            System.out.println("Press '2' if you want to play for '0'");
+            String symbols = scanner.nextLine().trim();
+            regexp = Pattern.compile("[12]?");
+            matcher = regexp.matcher(symbols);
+            if (matcher.matches() && !symbols.equals("")) {
+                playerNumber = parseInt(symbols);
+                switch (playerNumber) {
+                    case 1:
+                        System.out.println("You've chosen 'X'");
+                        isProceed = false;
+                        break;
+                    case 2:
+                        System.out.println("You've chosen '0'");
+                        isProceed = false;
+                        break;
+                }
+            } else {
+                System.out.println("Wrong input: it should be 1 or 2");
             }
         }
-        int horizontal;
-        int vertical;
+        System.out.println("To make a move You should type row and column through the \"space\"");
+        System.out.println("For example input can be: \"1 2\" or \"3 3\" etc.");
+        int row = 0;
+        int col = 0;
         if (playerNumber == 1) {
             while (true) {
                 System.out.println("Your move:");
-                horizontal = scanner.nextInt() - 1;
-                vertical = scanner.nextInt() - 1;
-                while (field[horizontal][vertical] != '.') {
-                    System.out.println("Field is not empty. Please choose another one:");
-                    horizontal = scanner.nextInt() - 1;
-                    vertical = scanner.nextInt() - 1;
-                }
-                field[horizontal][vertical] = 'X';
+                do {
+                    move = scanner.nextLine().trim().replaceAll(" +", " ");
+                    regexp = Pattern.compile("[123] [123]");
+                    if (!(regexp.matcher(move).matches())) {
+                        System.out.println("Wrong input. It should be \"raw col\"");
+                    } else {
+                        row = move.charAt(0) - 49;
+                        col = move.charAt(2) - 49;
+                        if (field[row][col] != '.') {
+                            System.out.println("The field is not empty. Please take another.");
+                        }
+                    }
+                } while (!(regexp.matcher(move).matches()) || field[row][col] != '.');
 
+                field[row][col] = 'X';
                 for (char[] chars : field) {
                     for (int j = 0; j < field.length; j++) {
                         System.out.print(chars[j] + " ");
@@ -98,13 +120,13 @@ public class TicTacToeRunner {
                     return;
                 }
                 System.out.println("\nComputer move:");
-                horizontal = (int) (Math.random() * 3);
-                vertical = (int) (Math.random() * 3);
-                while (field[horizontal][vertical] != '.') {
-                    horizontal = (int) (Math.random() * 3);
-                    vertical = (int) (Math.random() * 3);
+                row = (int) (Math.random() * 3);
+                col = (int) (Math.random() * 3);
+                while (field[row][col] != '.') {
+                    row = (int) (Math.random() * 3);
+                    col = (int) (Math.random() * 3);
                 }
-                field[horizontal][vertical] = '0';
+                field[row][col] = '0';
                 for (char[] chars : field) {
                     for (int j = 0; j < field.length; j++) {
                         System.out.print(chars[j] + " ");
@@ -147,13 +169,13 @@ public class TicTacToeRunner {
         } else {
             while (true) {
                 System.out.println("\nComputer move:");
-                horizontal = (int) (Math.random() * 3);
-                vertical = (int) (Math.random() * 3);
-                while (field[horizontal][vertical] != '.') {
-                    horizontal = (int) (Math.random() * 3);
-                    vertical = (int) (Math.random() * 3);
+                row = (int) (Math.random() * 3);
+                col = (int) (Math.random() * 3);
+                while (field[row][col] != '.') {
+                    row = (int) (Math.random() * 3);
+                    col = (int) (Math.random() * 3);
                 }
-                field[horizontal][vertical] = 'X';
+                field[row][col] = 'X';
                 for (char[] chars : field) {
                     for (int j = 0; j < field.length; j++) {
                         System.out.print(chars[j] + " ");
@@ -205,14 +227,22 @@ public class TicTacToeRunner {
                     return;
                 }
                 System.out.println("\nYour move:");
-                horizontal = scanner.nextInt() - 1;
-                vertical = scanner.nextInt() - 1;
-                while (field[horizontal][vertical] != '.') {
-                    System.out.println("Field is not empty. Please choose another one:");
-                    horizontal = scanner.nextInt() - 1;
-                    vertical = scanner.nextInt() - 1;
-                }
-                field[horizontal][vertical] = '0';
+
+                do {
+                    move = scanner.nextLine().trim().replaceAll(" +", " ");
+                    regexp = Pattern.compile("[123] [123]");
+                    if (!(regexp.matcher(move).matches())) {
+                        System.out.println("Wrong input. It should be \"raw col\"");
+                    } else {
+                        row = move.charAt(0) - 49;
+                        col = move.charAt(2) - 49;
+                        if (field[row][col] != '.') {
+                            System.out.println("The field is not empty. Please take another.");
+                        }
+                    }
+                } while (!(regexp.matcher(move).matches()) || field[row][col] != '.');
+
+                field[row][col] = '0';
                 for (char[] chars : field) {
                     for (int j = 0; j < field.length; j++) {
                         System.out.print(chars[j] + " ");
